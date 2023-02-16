466c443a272f01f8da7a0042c9e40c2ccd645cd9
laksjdjf <aikasama@hotmail.co.jp>
2023-02-16 16:25:00 +0900

    Update tagger_control.py

--- preprocess/tagger_control.py
+++ preprocess/tagger_control.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from preprocess import dbimutils
+import dbimutils
 
 from tensorflow.keras.models import load_model, Model
 
