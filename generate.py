4f4e7722b2557e4b98e2b738c9b3a8ec405e5217
laksjdjf <aikasama@hotmail.co.jp>
2023-02-17 10:37:01 +0900

    batch size、xformers、コマンドライン引数、()[]みたいなの対応

--- generate.py
+++ generate.py
@@ -5,12 +5,24 @@ from PIL import Image
 import tensorflow as tf
 import torch
 
-#設定
-model_id = "tintin-diffusion"
-pfg_path = "manman-pfg.pt"
-input_size = 768
-cross_attention_dim = 1024
-num_tokens = 5
+import argparse
+
+###コマンドライン引数#########################################################################
+parser = argparse.ArgumentParser(description='生成')
+parser.add_argument('--model_id', '-m', type=str, required=True, help='モデルパス（diffusers）')
+parser.add_argument('--pfg_path', '-p', type=str, required=True, help='pfgパス')
+parser.add_argument('--input_size', '-i', type=int, default = 768, help='分類器の出力、wdtaggerは768')
+parser.add_argument('--cross_attention_dim', '-c', type=int, default = 1024, help='v1は768, v2は1024')
+parser.add_argument('--num_tokens', '-n', type=int, default= 10, help='pfgトークン数')
+parser.add_argument('--share', '-s', action="store_true", help='gradio share機能を使う')
+args = parser.parse_args()
+############################################################################################
+
+model_id = args.model_id
+pfg_path = args.pfg_path
+input_size = args.input_size
+cross_attention_dim = args.cross_attention_dim
+num_tokens = args.num_tokens
 
 
 #なんもいみわかっとらんけどこれしないとVRAMくわれる。対応するバージョンもよくわからない
@@ -37,52 +49,60 @@ def infer(img:Image):
     return torch.tensor(probs.numpy()).unsqueeze(0)
   
 #Stable diffusion モデルのロード
-from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
 device = "cuda"
 
-pipe = StableDiffusionPipeline.from_pretrained(
+pipe = DiffusionPipeline.from_pretrained(
     model_id,
     torch_dtype=torch.float16,
+    custom_pipeline="lpw_stable_diffusion", #なんか知らんがwebui風のプロンプト強調()[]ができるらすい
 ).to(device)
+
+try:
+    pipe.unet.set_use_memory_efficient_attention_xformers(True)
+    print("apply xformers for unet !!!")
+except:
+    print("cant apply xformers. using normal unet !!!")
 
 from pfg.pfg import PFGNetwork
 pfg = PFGNetwork(pipe.unet, input_size, cross_attention_dim, num_tokens)
 pfg.load_state_dict(torch.load(pfg_path))
 pfg.requires_grad_(False)
 pfg.to(device, dtype = torch.float16)
-print("loaded")
+print("pfg loaded")
 
 import gradio as gr
 from datetime import datetime
 
 
-def generate(image,prompt,negative_prompt,image_scale,width,height,steps,guidance_scale):
+def generate(image,prompt,negative_prompt,batch_size,image_scale,width,height,steps,guidance_scale):
     hidden_states = infer(image).to(device)
     pfg.set_input(hidden_states * image_scale)
     with torch.autocast("cuda"):
-        images = pipe(prompt = prompt,
+        images = pipe(prompt = [prompt]*batch_size,
                       width = width,
                       height = height,
                       guidance_scale=guidance_scale,
                       num_inference_steps=steps,
-                      negative_prompt = negative_prompt,
+                      negative_prompt = [negative_prompt]*batch_size,
                       ).images
-    return images[0]
+    return images
 
 demo = gr.Interface(
     generate,
     [gr.Image(type="pil"),
      gr.Textbox(value="illustration of"),
      gr.Textbox(value="worst quality, low quality, medium quality, deleted, lowres, comic, bad anatomy,bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, signature, watermark, username, blurry"),
-     gr.Slider(0,2,value=1,step=0.01,label="pfgの強弱(？)"),
+     gr.Slider(1,32,value=1,step=1),
+     gr.Slider(0,2,value=1,step=0.01,label="pfgの強弱：ちょっとあげるだけでいい結果にならない"),
      gr.Slider(256,896,value=640,step=64),
      gr.Slider(256,896,value=896,step=64),
      gr.Slider(0,50,value=50,step=1),
-     gr.Slider(0,30,value=11,step=0.5)],
-    ["image"],
+     gr.Slider(0,30,value=12,step=0.5)],
+    [gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=[2], height="auto")],
     title = "タイトル",
     description = "説明",
     allow_flagging='never'
 )
 
-demo.launch(share=True)
+demo.launch(share = args.share)
